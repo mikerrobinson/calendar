94a399b9846917c3f474c667bfe2d8612066c431
Michael Robinson <mrobinson@godaddy.com>
2021-10-28 14:39:39 -0700

    initial commit

---
+++ Calendar/Controllers/CalendarController.cs
@@ -0,0 +1,31 @@
+﻿using Calendar.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calendar.Controllers
+{
+  public class CalendarController : Controller
+  {
+    private readonly ILogger<HomeController> _logger;
+
+    public CalendarController(ILogger<HomeController> logger)
+    {
+      _logger = logger;
+    }
+
+    public IActionResult Index()
+    {
+      return View();
+    }
+
+    public IActionResult Privacy()
+    {
+      return View();
+    }
+  }
+}
---
+++ README.md
@@ -0,0 +1,4 @@
+# Calendar POC
+
+This app will render a simple read-only monthly calendar within a browser window.  It's expected to be used from Smart Displays around a house and hooked up to, say, a family calendar.
+
