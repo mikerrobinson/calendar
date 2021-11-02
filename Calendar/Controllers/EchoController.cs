1125d980fbb7c63408322cdf0ec564e30ea87d36
Michael Robinson <mrobinson@godaddy.com>
2021-11-01 19:20:40 -0700

    Add in identity code

---
+++ Calendar/Controllers/EchoController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
+
+namespace Calendar.Controllers
+{
+  public class EchoController : Controller
+  {
+    private readonly IConfiguration _configuration;
+
+    public EchoController(ILogger<HomeController> logger, IConfiguration configuration)
+    {
+      _configuration = configuration;
+    }
+
+    public IActionResult Index()
+    {
+      return View(_configuration.AsEnumerable());
+    }
+  }
+}
---
+++ Calendar/Models/AuthenicationSecrets.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Calendar.Models
+{
+  public class AuthenicationSecrets
+  {
+    public string GoogleClientId { get; set; }
+    public string GoogleClientSecret { get; set; }
+  }
+}
